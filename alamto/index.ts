b87208dbac290e81d504d07087a4fa7c0643d767
ArsalanNury <arsiiiland@yahoo.com>
2024-01-23 23:11:34 +0330

    alamto scrapping completed

--- alamto/index.ts
+++ alamto/index.ts
@@ -9,44 +9,61 @@ const scrap = async () => {
     headless: false,
   });
 
-  const page = await browser.newPage();
-  await page.goto(url);
+    for (let index = 0; index < 10; index++) {
+         const page = await browser.newPage();
+         await page.goto(url);
 
-  await page.setViewport({ height: 1000, width: 900 });
-  await page.waitForSelector(".post-content p:nth-child(7)");
-  await page.$eval(".post-content p:nth-child(7) a", (e) =>
-    e.setAttribute("target", "_self")
-  );
-  await page.click(".post-content p:nth-child(7)");
-  await page.waitForSelector(".sub-title", { timeout: 600000 });
+         await page.setViewport({ height: 1000, width: 900 });
+         await page.waitForSelector(".post-content p:nth-child(7)");
+         await page.$eval(".post-content p:nth-child(7) a", (e) =>
+           e.setAttribute("target", "_self")
+         );
+         await page.click(".post-content p:nth-child(7)");
+         await page.waitForSelector(".sub-title", { timeout: 600000 });
 
- const scrapping = await page.evaluate(() => {
-    const title = (
-      document.querySelector(
-        ".post-content p:nth-child(7)"
-      ) as HTMLParagraphElement
-    ).innerText;
-    const result = (
-      document.querySelector(
-        ".boxpopular p:nth-child(2)"
-      ) as HTMLParagraphElement
-    ).innerText;
-   const poetries = document.querySelectorAll(".b");
-   
-    console.log(poetries, "poetries");
-   
-  });
+         const scrapping = await page.evaluate(() => {
+           const title = (
+             document.querySelector(
+               ".post-content p:nth-child(7)"
+             ) as HTMLParagraphElement
+           ).innerText;
+           const result = (
+             document.querySelector(
+               ".boxpopular p:nth-child(2)"
+             ) as HTMLParagraphElement
+           ).innerText;
 
-  // const readFile = fs.readFileSync("./result.json", { encoding: "utf8" });
+           const arr: any[] = [];
+           const rightPoetries = Array.from(
+             document.querySelectorAll(".b .m1")
+           );
+           const leftPoetries = Array.from(document.querySelectorAll(".b .m2"));
+           const poetries = document.querySelectorAll(".b");
 
-  // const fileToJson = JSON.parse(readFile);
-  // fileToJson.push(scrapping);
+           for (let index = 0; index < poetries.length; index++) {
+             arr.push({ peotry: rightPoetries[index].textContent });
+             arr.push({ peotry: leftPoetries[index].textContent });
+           }
 
-  // fs.writeFileSync("./result.json", JSON.stringify(fileToJson));
+           return {
+             title,
+             poetries: arr,
+             peotryTranslate: result,
+           };
+         });
 
-  setTimeout(async () => {
-    await browser.close();
-  }, 60000);
+         const readFile = fs.readFileSync("./result.json", {
+           encoding: "utf8",
+         });
+
+         const fileToJson = JSON.parse(readFile);
+         fileToJson.push(scrapping);
+
+         fs.writeFileSync("./result.json", JSON.stringify(fileToJson));
+      
+    }
+
+    browser.close();
 };
 
 scrap();
